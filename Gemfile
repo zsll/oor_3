c3a8dd16dfe6aa5e0e375d7c7a94b740aa7504f3
Yangzi Liu <asu.tristan@gmail.com>
2013-06-23 14:06:33 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'highlight', :require => 'simplabs/highlight'
+gem 'twitter-bootstrap-rails' #run 'rails generate bootstrap:install static' after 'bundle install'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oor3::Application.config.secret_token = '78d9de47bca119d654ca4c8feb5467f7711cc73f00b9eb880d91bb715a7173ee442e294ba2aedf80a1727ddc6886c7e1f9846a13f29a41fac2d49d0ee5bcf9b8'
